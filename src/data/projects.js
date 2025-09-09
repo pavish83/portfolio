b10221ea9ec54a0f7b15bdbbb9632e61b34bab90
Pavel Vishnepolsky <pavish83@gmail.com>
2025-09-09 13:25:39 -0400

    update text

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 const projects = [
   {
     id: 1,
-    nameKey: "Store application",
+    nameKey: "Store management application",
     descriptionKey: "A storage management and file sharing platform that lets users effortlessly upload, organize, and share files. Built with the latest Next.js 15 and the Appwrite Node SDK, utilizing advanced features for seamless file management.",
     featuresKeys: [
       "User Authentication with Appwrite: Implement signup, login, and logout functionality using Appwrite's authentication system.",
